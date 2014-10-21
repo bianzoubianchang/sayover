07ea4af6a2612391175fc3e7467a2e9b123111f0
longyu <yqcui669@126.com>
2014-10-21 19:56:15 +0800

    few alter.

--- src/anttest/helloworld.java
+++ src/anttest/helloworld.java
@@ -5,12 +5,13 @@ import org.testng.annotations.*;
 public class helloworld {
 	public static void main(String[] args){
 		System.out.println("Hello world!");
-
+                System.out.println("Say Goodbye!");
 	}
 	
 @Test
 	public void testecho(){
 		System.out.println("Hello world!");
+		Systen.out.println("Apple!");
 	}
 	
 
