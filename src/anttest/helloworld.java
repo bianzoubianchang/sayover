bfe3643c5b2187bb0e5a5f24b371e892aa97f1eb
longyu <yqcui669@126.com>
2014-10-21 18:32:41 +0800

    The first commit.

---
+++ src/anttest/helloworld.java
@@ -0,0 +1,17 @@
+package anttest;
+
+import org.testng.annotations.*;
+
+public class helloworld {
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+
+	}
+	
+@Test
+	public void testecho(){
+		System.out.println("Hello world!");
+	}
+	
+
+}
