70c92234325fc90209b8df80ab13292a534941d9
longyu <yqcui669@126.com>
2014-10-29 16:21:55 +0800

    commit for testing.

--- src/anttest/helloworld.java
+++ src/anttest/helloworld.java
@@ -5,13 +5,12 @@ import org.testng.annotations.*;
 public class helloworld {
 	public static void main(String[] args){
 		System.out.println("Hello world!");
-                System.out.println("Say Goodbye!");
+
 	}
 	
 @Test
 	public void testecho(){
 		System.out.println("Hello world!");
-		Systen.out.println("Apple!");
 	}
 	
 
